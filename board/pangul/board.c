54b8f1200288d3133b4b594991410c53d48115a9
yuan.huang2 <yuan.huang2@bitland.com.cn>
2021-01-08 10:58:11 +0800

    pangu-l: Modify fan rpm_min.
    Modify fan rpm_min.
    
    BUG=none
    BRANCH=pangul-dev-branch
    TEST=HOST_CROSS_COMPILE= CROS_COMPILE_arm=arm-none-eabi- make -j32 BOARD=pangul,
    Compile successfully.
    
    Change-Id: I8be2082c15dcd03bc553afeb3a00d665a91deb63

--- board/pangul/board.c
+++ board/pangul/board.c
@@ -341,14 +341,14 @@ const struct fan_conf fan_conf_1 = {
 };
 
 const struct fan_rpm fan_rpm_0 = {
-	.rpm_min = 1000,
-	.rpm_start = 1000,
+	.rpm_min = 220,
+	.rpm_start = 220,
 	.rpm_max = 2500,
 };
 
 const struct fan_rpm fan_rpm_1 = {
-	.rpm_min = 1000,
-	.rpm_start = 1000,
+	.rpm_min = 220,
+	.rpm_start = 220,
 	.rpm_max = 2500,
 };
 
