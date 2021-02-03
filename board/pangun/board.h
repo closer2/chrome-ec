97869d9b50c755f443e9bf4feeb9324fcd19d506
yuan.huang2 <yuan.huang2@bitland.com.cn>
2021-02-03 09:33:54 +0800

    pangu-n: Create pangun board.
    Create pangun board for DT02
    
    BUG=none
    BRANCH=pangul-dev-branch
    TEST=HOST_CROSS_COMPILE= CROS_COMPILE_arm=arm-none-eabi- make -j32 BOARD=pangun,
    Compile successfully.
    
    Change-Id: Ifb931e280d1ec250baed986fedbfd0c938c6667f

--- board/pangun/board.h
+++ board/pangun/board.h
@@ -12,11 +12,11 @@
  * platform config.
  * choose one platform {PLATFORM_CONFIGURATION_DT01, PLATFORM_CONFIGURATION_DT02}
  * ----------------------------------------------------------------------------*/
-#define PLATFORM_CONFIGURATION_DT01
+#define PLATFORM_CONFIGURATION_DT02
 
 /* Features depend on chip family */
-#if defined(PLATFORM_CONFIGURATION_DT01)
-#define NPCX_FAMILY_DT01
+#if defined(PLATFORM_CONFIGURATION_DT02)
+#define NPCX_FAMILY_DT02
 #else
 #error "Unsupported platform family"
 #endif
@@ -35,12 +35,12 @@
 *            EC test version for ODM debug
 ------------------------------------------------------------------------------*/
 #define BLD_EC_VERSION_X        "0"
-#define BLD_EC_VERSION_YZ       "03"
-#define BLD_EC_VERSION_TEST     "04"
+#define BLD_EC_VERSION_YZ       "02"
+#define BLD_EC_VERSION_TEST     "00"
 
 #define BLD_EC_VERSION_X_HEX    0x00
-#define BLD_EC_VERSION_YZ_HEX   0x03
-#define BLD_EC_VERSION_TEST_HEX 0x04
+#define BLD_EC_VERSION_YZ_HEX   0x02
+#define BLD_EC_VERSION_TEST_HEX 0x00
 
 /*------------------------------------------------------------------------------
 * NPCX7 config
