f1ef662a32e78cae207b42e5b832cb96f08afb4a
morgen <xiaoqiang.zhu@bitland.com.cn>
2021-02-03 14:15:02 +0800

    pangu-n: config I2C port 0 for HC32F460
    config I2C port 0(GPIOB4/B5) for HC32F460 Firmware update
    
    BUG=none
    BRANCH=pangul-dev-branch
    TEST= 1. Compile successfully.
    
    Change-Id: Ic1250703638e28daa1643b401a4d1b4ef2a303af

--- board/pangun/board.c
+++ board/pangun/board.c
@@ -323,6 +323,13 @@ BUILD_ASSERT(ARRAY_SIZE(thermal_params) == TEMP_SENSOR_COUNT);
 
 /* TODO: check with real hardware, this is error */
 const struct i2c_port_t i2c_ports[] = {
+    {
+        .name = "hc32F460",
+        .port = I2C_PORT_HC32F460,
+        .kbps = 400,
+        .scl = GPIO_F460_PA2_CLK,
+        .sda = GPIO_F460_PA3_DAT,
+    },
 	{
 		.name = "smb-ext",
 		.port = I2C_PORT_SMB_AUX,
