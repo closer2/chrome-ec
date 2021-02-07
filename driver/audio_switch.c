94fe27c8f015e8eabe6450ce3cd431a1c991bce1
morgen <xiaoqiang.zhu@bitland.com.cn>
2021-02-07 15:14:58 +0800

    audio : modify audio switch function
    EC must detect front and rear headset insert status by read
    GPIO_FRONT_HP_JD_INPUT and GPIO_REAR_HP_JD_INPUT.
    
    EC pull down GPIO_MUX_SWITCH_OUTPUT when front headset detected, and
    pull up when rear headset detected.
    
    EC must pull down GPIO_JD_OUT_ALC256 when headset are detected.
    EC must pull up GPIO_JD_OUT_ALC256 when no headphones were detected.
    
    EC must send a high pulse(1s) on GPIO_JD_OUT_ALC256 when another
    headset is detected.
    
    Front headset preferred
    
    BUG=none
    BRANCH=pangul-dev-branch
    TEST= 1. Compile successfully.
          2. function verify pass
    
    Change-Id: I5368751903666cb58f5fd55e62717e8d3164cc79

--- driver/audio_switch.c
+++ driver/audio_switch.c
@@ -29,6 +29,21 @@
 static int detect_result = 0;
 static int detect_result_backup = 0;
 
+
+/* EC must detect front and rear headset insert status by read 
+ * GPIO_FRONT_HP_JD_INPUT and GPIO_REAR_HP_JD_INPUT.
+ *
+ * EC pull down GPIO_MUX_SWITCH_OUTPUT when front headset detected, and
+ * pull up when rear headset detected.
+ *
+ * EC must pull down GPIO_JD_OUT_ALC256 when headset are detected.
+ * EC must pull up GPIO_JD_OUT_ALC256 when no headphones were detected.
+ *
+ * EC must send a high pulse(1s) on GPIO_JD_OUT_ALC256 when another 
+ * headset is detected.
+ *
+ * Front headset preferred
+ */
 static void headset_detect(void)
 {
     if(POWER_S0 != power_get_state()) {
@@ -39,34 +54,39 @@ static void headset_detect(void)
         
         return;
     }
-    
-    if(gpio_get_level(GPIO_FRONT_HP_JD_INPUT)) {
+
+    /* front headset insert */
+    if(!gpio_get_level(GPIO_FRONT_HP_JD_INPUT)) {
         detect_result |= 0x01;
     } else {
         detect_result &= (~0x01);
     }
 
-    if(gpio_get_level(GPIO_REAR_HP_JD_INPUT)) {
+    /* rear headset insert */
+    if(!gpio_get_level(GPIO_REAR_HP_JD_INPUT)) {
         detect_result |= 0x02;
     } else {
         detect_result &= (~0x02);
     }
 
     if(detect_result != detect_result_backup) {
-        detect_result_backup = detect_result;
 
+        /* don't notify ALC256 when front headset stabilization */
+        gpio_set_level(GPIO_JD_OUT_ALC256, (detect_result_backup&0x01)?0:1);
+        msleep(1000);
+        gpio_set_level(GPIO_JD_OUT_ALC256, detect_result?0:1);
+
+        /* pull down when front headset detected */
         gpio_set_level(GPIO_MUX_SWITCH_OUTPUT, (detect_result&0x01)?0:1);
-        msleep(10);
-        
-        gpio_set_level(GPIO_JD_OUT_ALC256, 1);
-        msleep(10);
-        gpio_set_level(GPIO_JD_OUT_ALC256, 0);
 
-        CPRINTS("Front HP_JD=%d, Rear HP_JD=%d, Switch=%d, detect_result=%X",
+        CPRINTS("Front HP_JD=%d, Rear HP_JD=%d, Switch=%d, JD_ALC256=%d, detect_result=%X",
             gpio_get_level(GPIO_FRONT_HP_JD_INPUT),
             gpio_get_level(GPIO_REAR_HP_JD_INPUT),
             gpio_get_level(GPIO_MUX_SWITCH_OUTPUT),
+            gpio_get_level(GPIO_JD_OUT_ALC256),
             detect_result);
+
+        detect_result_backup = detect_result;
     }
 }
 DECLARE_HOOK(HOOK_SECOND, headset_detect, HOOK_PRIO_DEFAULT);
