2a8b1c7bae5fe94251dd2d4a657fbdc4b82f05d6
yuan.huang2 <yuan.huang2@bitland.com.cn>
2021-03-01 09:34:28 +0800

    pangu-l: DT01-V051 release.
    Official release.
    
    BUG=none
    BRANCH=pangul-dev-branch
    TEST=HOST_CROSS_COMPILE= CROS_COMPILE_arm=arm-none-eabi- make -j32 BOARD=pangul,
    Compile successfully.
    
    Change-Id: I84665f5808e8cf3f1aeca9e6b8680814c7a2c882

--- board/pangul/board.h
+++ board/pangul/board.h
@@ -35,12 +35,12 @@
 *            EC test version for ODM debug
 ------------------------------------------------------------------------------*/
 #define BLD_EC_VERSION_X        "0"
-#define BLD_EC_VERSION_YZ       "50"
-#define BLD_EC_VERSION_TEST     "01"
+#define BLD_EC_VERSION_YZ       "51"
+#define BLD_EC_VERSION_TEST     "00"
 
 #define BLD_EC_VERSION_X_HEX    0x00
-#define BLD_EC_VERSION_YZ_HEX   0x50
-#define BLD_EC_VERSION_TEST_HEX 0x01
+#define BLD_EC_VERSION_YZ_HEX   0x51
+#define BLD_EC_VERSION_TEST_HEX 0x00
 
 /*------------------------------------------------------------------------------
 * NPCX7 config
