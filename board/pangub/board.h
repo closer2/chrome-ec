fbd9101ef9de79b474ff8b8c9f4fb78446bd13b7
hao.zhou3 <hao.zhou3@bitland.com.cn>
2021-06-15 10:33:37 +0800

    pangu-b: DT03-V061 release.
    Official release.
    
    BUG=none
    BRANCH=pangul-dev-branch
    TEST=HOST_CROSS_COMPILE= CROS_COMPILE_arm=arm-none-eabi- make -j32 BOARD=pangub,
    Compile successfully.
    
    Change-Id: Ie8891cb3300bdc88cbf477ccf1395a9ab60a8bf4

--- board/pangub/board.h
+++ board/pangub/board.h
@@ -37,11 +37,11 @@
 *            EC test version for ODM debug
 ------------------------------------------------------------------------------*/
 #define BLD_EC_VERSION_X        "0"
-#define BLD_EC_VERSION_YZ       "60"
+#define BLD_EC_VERSION_YZ       "61"
 #define BLD_EC_VERSION_TEST     "00"
 
 #define BLD_EC_VERSION_X_HEX    0x00
-#define BLD_EC_VERSION_YZ_HEX   0x60
+#define BLD_EC_VERSION_YZ_HEX   0x61
 #define BLD_EC_VERSION_TEST_HEX 0x00
 
 /*------------------------------------------------------------------------------
